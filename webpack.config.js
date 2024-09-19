7b51c613bb34c9041288b20b6f6f3174f16df8de
gjsjohnmurray <johnm@georgejames.com>
2024-09-19 21:19:45 +0100

    Fix packaging

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const extensionConfig = {
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file,
-    'swagger-ui-dist': 'commonjs swagger-ui-dist'
+    '@vscode-elements/elements': 'commonjs @vscode-elements/elements'
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
